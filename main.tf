2b97725becb79649cc43d89cd804e85baaab0e5a
Diamondd01 <Nadiamondj@gmail.com>
2024-07-19 14:21:25 -0400

    updated terraform

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
 # Provider (OCI)
 provider "oci" {
-    tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa52c7qr7y3rm5qveso7y4jn3evpgk6bss6nndfzoqum3tftibwuhq"
-    private_key_path =  "C:\Users\dijewell\Downloads\ssh-key-2024-07-19.key.crdownload"
-    region = "US East (Ashburn)"
+    tenancy_ocid = var.tenancy_ocid
+    private_key_path =  var.private_key_path
+    region = var.region
 }
 # Define VCN
 resource "oci_core_virtual_network" "CI-CD-VCN"{ 
-    cidr_block: "10.0.0.0/16"
+    cidr_block: var.vcn_cidr_block
 }
 # define a subnet within vcn 
 resource "oci_core_virtual_subnet" "public subnet-CI-CD-VCN" { 
-    cidr_block = "10.0.0.0/24"
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaarlony4tu43mq25zkj6frzk3zid43hox5wwmiyfciebe6szqo7cvq"
-    vcn_id = "ocid1.vcn.oc1.iad.amaaaaaajtkboyqa74szdvgnfl2os7rx54oakv3bbyxmnna52qlosg7uwrgq"
+    cidr_block = var.subnet_cidr_block
+    compartment_id = var.compartment_id
+    vcn_id = var.vcn_id
     display_name =  "public subnet-CI-CD-VCN"
 }    
 # define a compute instance
  resource "oci_core_instance" "instance-CICD-OCI"{
-    availability_domain ="AD-1"
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaarlony4tu43mq25zkj6frzk3zid43hox5wwmiyfciebe6szqo7cvq"
-    shape = "VM.Standard.E4.Flex"
-    subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaampdltxvfftn2s7ljyxv7f6rxs7lc2rktqq7uglog37d2yqgyqfhq"
-
+    availability_domain =var.availability_domain
+    compartment_id = var.compartment_id
+    shape = var.shape
+    subnet_id = var.subnet_id
 # metadata for the instance 
     metadata {
         ssh_authorized_keys: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCx+V+Z0D/eaxICN3heR3f3OyFTkQ2NaUTlMtY3cv7MA"
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "tenancy_ocid" {
+  description = "ocid1.tenancy.oc1..aaaaaaaa52c7qr7y3rm5qveso7y4jn3evpgk6bss6nndfzoqum3tftibwuhq"
+}
+variable "private_key_path"{
+    description:"C:\Users\dijewell\Downloads\ssh-key-2024-07-19.key.crdownload"
+}
+variable "region"{
+    description: "us-auburn-1"
+}
+
+variable "compartment_id"{
+    description:"ocid1.compartment.oc1..aaaaaaaarlony4tu43mq25zkj6frzk3zid43hox5wwmiyfciebe6szqo7cvq"
+}
+variable "availability_domain"{
+    description:"OCI AD-1"
+}
+variable "shape"{
+    description: "VM.Standard.E4.Flex"
+}
+variable "vcn_cidr_block"{
+    description:"10.0.0.0/24"
+}
+variable "subnet_cidr_block"{
+    description:"10.0.0.0/24"
+}
+variable "vcn_id"{
+    description:"ocid1.vcn.oc1.iad.amaaaaaajtkboyqa74szdvgnfl2os7rx54oakv3bbyxmnna52qlosg7uwrgq"
+}
