6415adafad5ed323a674ea1de729a15d6c83ca9c
Oracle Public Cloud User <opc@cicd-oci.sub07191622530.cicdvcn.oraclevcn.com>
2024-07-22 22:53:01 +0000

    ci/cd project

---
+++ .vscode-server/data/User/History/4210e4ba/hvDT.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const apiKey='085a2baf803d961034e4af8cf025ff07'
+    const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+
+    const weatherForm = document.getElementById("weatherForm");
+    const weatherInfo = document.getElementById("weatherInfo");
+
+    weatherForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        const cityId =  document.getElementById('cityIdInput').value;
+
+        fetchWeatherByCity(cityId);
+    });
+
+    function fetchWeatherByCity(cityId){
+        const apiUrl = `${baseUrl}?id=${cityId}&appid=${apiKey}`;
+
+        fetch(apiUrl)
+            .then(response => {
+                if(!response.ok){
+                    throw new Error("Network response was not ok")
+                }
+                return response.json();
+            })
+            .then(data => {
+                const forecasts = data.list.slice(0,5);
+                const forecastHtml = forecasts.map(forecast =>{
+                    return `<p>${forecast.dt_txt}: ${forecast.main.temp} °C - ${forecast.weather[0].description}</p>`
+                }).join('');
+
+                weatherInfo.innerHTML= `<h2>Weather Forecast for City ${cityId}</h2>${forecastHtml}`;
+            })
+            .catch(error => {
+                console.error("Fetch Error",error)
+                weatherInfo.innerHTML= `<p>Failed to fetch weather data for ${city}. Please Try Again. </p>`
+            });
+    }
+})
