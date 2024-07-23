b6c2a7fa6b3b56c3d85e99193b2f29c05a6aae0b
Diamondd01 <Nadiamondj@gmail.com>
2024-07-23 10:30:42 -0400

    updated variables

--- main.tf
+++ main.tf
@@ -8,13 +8,7 @@ provider "oci" {
 resource "oci_core_virtual_network" "CI-CD-VCN"{ 
     cidr_block=var.vcn_cidr_block
     compartment_id= var.compartment_id
-}
-# define a subnet within vcn 
-resource "oci_core_subnet" "CICD_VCN_Subnet" { 
-    cidr_block = var.subnet_cidr_block
-    compartment_id = var.compartment_id
-    vcn_id = var.vcn_id
-}    
+}   
 # define a compute instance
  resource "oci_core_instance" "instance-CICD-Project1"{
     availability_domain =var.availability_domain
